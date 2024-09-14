0527aeb1cf9a5fedacd9f35452c53610a45cafd4
Antony Vilanova <115712032+AntonyVilanovaSilva@users.noreply.github.com>
2024-09-14 09:31:37 -0300

    14/09

---
+++ SAEP-SQL/TaskToDo/WebApplication1/Controllers/TarefasController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TarefasController : Controller
+    {
+        private BdToDoEntities db = new BdToDoEntities();
+
+        // GET: Tarefas
+        public ActionResult Index()
+        {
+            var tarefa = db.Tarefa.Include(t => t.StatusTarefa).Include(t => t.Usuário);
+            return View(tarefa.ToList());
+        }
+
+        // GET: Tarefas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarefa tarefa = db.Tarefa.Find(id);
+            if (tarefa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tarefa);
+        }
+
+        // GET: Tarefas/Create
+        public ActionResult Create()
+        {
+            ViewBag.IDStatus = new SelectList(db.StatusTarefa, "ID", "Descricao");
+            ViewBag.IDUsuario = new SelectList(db.Usuário, "ID", "Nome");
+            return View();
+        }
+
+        // POST: Tarefas/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Data,Descricao,IDStatus,IDUsuario")] Tarefa tarefa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tarefa.Add(tarefa);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IDStatus = new SelectList(db.StatusTarefa, "ID", "Descricao", tarefa.IDStatus);
+            ViewBag.IDUsuario = new SelectList(db.Usuário, "ID", "Nome", tarefa.IDUsuario);
+            return View(tarefa);
+        }
+
+        // GET: Tarefas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarefa tarefa = db.Tarefa.Find(id);
+            if (tarefa == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IDStatus = new SelectList(db.StatusTarefa, "ID", "Descricao", tarefa.IDStatus);
+            ViewBag.IDUsuario = new SelectList(db.Usuário, "ID", "Nome", tarefa.IDUsuario);
+            return View(tarefa);
+        }
+
+        // POST: Tarefas/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Data,Descricao,IDStatus,IDUsuario")] Tarefa tarefa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tarefa).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IDStatus = new SelectList(db.StatusTarefa, "ID", "Descricao", tarefa.IDStatus);
+            ViewBag.IDUsuario = new SelectList(db.Usuário, "ID", "Nome", tarefa.IDUsuario);
+            return View(tarefa);
+        }
+
+        // GET: Tarefas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarefa tarefa = db.Tarefa.Find(id);
+            if (tarefa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tarefa);
+        }
+
+        // POST: Tarefas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tarefa tarefa = db.Tarefa.Find(id);
+            db.Tarefa.Remove(tarefa);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
